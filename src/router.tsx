87938694c1b7daeaa96faf26d854ccf2ba2750af
Nauja <jeremymorosi@hotmail.fr>
2026-06-08 19:08:44 +0200

    Add GTM

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,31 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Layout from './components/Layout'
 import Library from './pages/Library'
 import Generate from './pages/Generate'
 import StoryView from './pages/StoryView'
 import Settings from './pages/Settings'
 
+function GTMPageTracker() {
+  const location = useLocation()
+
+  useEffect(() => {
+    const dl = (window as any).dataLayer
+    if (!dl) return
+    dl.push({
+      event: 'page_view',
+      page_path: location.pathname,
+      page_title: document.title,
+    })
+  }, [location.pathname])
+
+  return null
+}
+
 export default function Router() {
   return (
     <HashRouter>
+      <GTMPageTracker />
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Library />} />
